4583ce30ef6e1c85292a52c95ac87cafc067c9fb
Louis <lfpate11@gmail.com>
2025-11-12 17:45:09 -0500

    started adding other auth routes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,6 +3,7 @@ import { ApiResponse } from '../utils/api-response.js';
 import { ApiError } from '../utils/api-errors.js';
 import { asyncHandler } from '../utils/async-handler.js';
 import { sendEmail, emailVerificationMailgenContent } from '../utils/mail.js';
+import crypto from 'crypto';
 
 const generateAccessAndRefreshTokens = async userId => {
   try {
@@ -147,4 +148,59 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, 'User logged out successfully'));
 });
 
-export { registerUser, generateAccessAndRefreshTokens, login, logoutUser };
+const currentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, 'Current user fetched successfully'));
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { verificationToken } = req.params;
+
+  if (!verificationToken) {
+    throw new ApiError(400, 'Verification token is missing');
+  }
+
+  let hashedToken = crypto
+    .createHash('sha256')
+    .update(verificationToken)
+    .digest('hex');
+
+  const user = await User.findOne({
+    emailVerificationToken: hashedToken,
+    emailVerificationExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(400, 'Token is invalid or expired');
+  }
+
+  user.emailVerificationToken = undefined;
+  user.emailVerificationExpiry = undefined;
+
+  user.isEmailVerified = true;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { isEmailVerified: true },
+        'Email verified successfully'
+      )
+    );
+});
+
+// const currentUser = asyncHandler(async (req, res) => {})
+
+// const currentUser = asyncHandler(async (req, res) => {})
+
+export {
+  registerUser,
+  generateAccessAndRefreshTokens,
+  login,
+  logoutUser,
+  currentUser,
+  verifyEmail,
+};
